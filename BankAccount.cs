15c1aa4458f513502a8d75bada2a7c46d0509f72
PaildarBasanthi <basanthisrec@gmail.com>
2023-07-26 15:52:19 +0530

    first Commit

---
+++ BankAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class BankAccount
+{
+    // Properties
+    public string AccountNumber { get; private set; }
+    public string AccountHolderName { get; private set; }
+    public double balance; // Private field to store the balance
+
+    // Constructor
+    public BankAccount(string accountHolderName)
+    {
+        AccountNumber = GenerateAccountNumber();
+        AccountHolderName = accountHolderName;
+        balance = 0;
+    }
+
+    // Method to generate a unique account number
+    private string GenerateAccountNumber()
+    {
+        // In a real-world scenario, this could be a more sophisticated algorithm.
+        // For this demonstration, we'll simply use a sequential number prefixed with "ACC".
+        string accountNumber = "ACC" + Guid.NewGuid().ToString("N").Substring(0, 6).ToUpper();
+        return accountNumber;
+    }
+
+    // Method to deposit an amount into the account
+    public void Deposit(double amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Deposit amount must be positive.");
+        }
+
+        balance += amount;
+    }
+
+    // Method to withdraw an amount from the account
+    public void Withdraw(double amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Withdrawal amount must be positive.");
+        }
+
+        if (amount > balance)
+        {
+            throw new InvalidOperationException("Insufficient funds.");
+        }
+
+        balance -= amount;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Creating a bank account and making some transactions
+        Console.WriteLine("Enter Account Holder Name");
+        string accountHolderName = Console.ReadLine();
+        BankAccount account = new BankAccount(accountHolderName);
+
+
+        Console.WriteLine($"Account Number: {account.AccountNumber}");
+        Console.WriteLine($"Account Holder Name: {account.AccountHolderName}");
+        Console.WriteLine($"Initial Balance: {account.balance}");
+
+        account.Deposit(1000);
+        Console.WriteLine($"Balance after deposit: {account.balance}");
+
+        account.Withdraw(500);
+        Console.WriteLine($"Balance after withdrawal: {account.balance}");
+
+        try
+        {
+            account.Withdraw(700); // This will throw an exception due to insufficient funds
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
+
